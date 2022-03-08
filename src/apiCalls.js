b822c8b149f3da2fe34c7e9212a390924b879c04
Phil Lewis <phillewis1985@gmail.com>
2022-03-08 09:53:19 -0700

    Build post request method

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,16 @@ export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
     .then(response => response.json())
 }
+
+export const makeNewOrder = (name, ingredients) => {
+  return fetch('http://localhost:3001/api/v1/orders', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: name,
+      ingredients: ingredients
+    })
+  }).then(response => response.json())
+}
